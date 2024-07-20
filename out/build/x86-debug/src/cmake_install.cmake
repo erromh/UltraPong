2f8c4a645037f4e767cfd6d8398ea7418d4d17e7
errommore995@gmail.com <errommore995@gmail.com>
2024-07-20 23:16:13 +0600

    test from win32

--- out/build/x86-debug/src/cmake_install.cmake
+++ out/build/x86-debug/src/cmake_install.cmake
@@ -34,7 +34,6 @@ endif()
 
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   # Include the install script for each subdirectory.
-  include("D:/c++++/UltraPong/out/build/x86-debug/src/app2/cmake_install.cmake")
   include("D:/c++++/UltraPong/out/build/x86-debug/src/Ball/cmake_install.cmake")
 
 endif()
